cb8c545f29cdbe96cb560c090dba1a326bf64d0c
Eldos106 <toktosunoveldos99@gmail.com>
2025-04-06 19:45:13 +0600

    dz-2

--- js/home_works.js
+++ js/home_works.js
@@ -13,22 +13,62 @@ emailBtn.onclick = () => {
         emailResult.style.color ='red'
     }
 }
-function moveBlock(block, parent, position = 0) {
-    let parentWidth = parent.clientWidth;
-    let blockWidth = block.clientWidth;
+const emailInput = document.querySelector('#gmail_input')
+const emailBtn = document.querySelector('#gmail_button')
+const emailResult = document.querySelector('#gmail_result')
 
-    if (position + blockWidth >= parentWidth) {
-        return;
+const regExs =  /^[a-zA-Z0-9._%+-]+@gmail\.com$/;
+
+emailBtn.onclick = () => {
+    if (regExs.test(emailInput.value)) {
+        emailResult.innerText = 'OK'
+        emailResult.style.color = 'green'
+    }else {
+        emailResult.innerText ='ERROR'
+        emailResult.style.color ='red'
     }
-
-    block.style.left = position + "px";
-
-    requestAnimationFrame(() => moveBlock(block, parent, position + 2));
 }
 
-window.onload = function () {
-    let smallBlock = document.querySelector(".child_block");
-    let parentBlock = document.querySelector(".parent_block");
+const parentBlock = document.querySelector('.parent_block');
+const childBlock = document.querySelector('.child_block');
 
-    moveBlock(smallBlock, parentBlock);
-};
+let positionX = 0
+let positionY = 0
+let direction = 'right' // Направление движения
+
+const offWidth = parentBlock.offsetWidth - childBlock.offsetWidth
+const offHeight = parentBlock.offsetHeight - childBlock.offsetHeight
+
+const moveBlock = () => {
+    if (direction === 'right') {
+        if (positionX < offWidth) {
+            positionX++
+        } else {
+            direction = 'down'
+        }
+    } else if (direction === 'down') {
+        if (positionY < offHeight) {
+            positionY++
+        } else {
+            direction = 'left'
+        }
+    } else if (direction === 'left') {
+        if (positionX > 0) {
+            positionX--
+        } else {
+            direction = 'up'
+        }
+    } else if (direction === 'up') {
+        if (positionY > 0) {
+            positionY--
+        } else {
+            direction = 'right'
+        }
+    }
+
+    childBlock.style.left = `${positionX}px`
+    childBlock.style.top = `${positionY}px`
+    requestAnimationFrame(moveBlock)
+}
+
+moveBlock();
